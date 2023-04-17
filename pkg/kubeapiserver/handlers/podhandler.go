546a9d6d9d098d8de2ebbfdaf480430f2cd5c855
zhaohaoru <zhaohaoru@sjtu.edu.cn>
2023-04-17 14:12:16 +0800

    test create pod and etcd function

--- pkg/kubeapiserver/handlers/podhandler.go
+++ pkg/kubeapiserver/handlers/podhandler.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"github.com/coreos/etcd/clientv3"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"minik8s/pkg/apiobject"
 	"minik8s/pkg/kubeapiserver/storage"
 	"net/http"
+	"regexp"
 )
 
 type PodHandler struct {
@@ -19,16 +21,42 @@ func NewPodHandler(client *clientv3.Client) *PodHandler {
 	}
 }
 
-// CreatePod the url format is POST /api/v1/namespaces/{namespace}/pods
-func CreatePod(c *gin.Context) {
+// CreatePod the url format is POST /api/v1/namespaces/:namespace/pods
+// TODO: bind the pod in runtime
+func (p *PodHandler) CreatePod(c *gin.Context) {
 	// 1. parse the request get the pod from the request
+	rawUrl := c.Request.URL.Path
+	r, err := regexp.Compile("/api/v1/namespaces/([^/]+)/pods")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	namespace := r.FindStringSubmatch(rawUrl)[1]
+	log.Debug("[CreatePod] namespace: ", namespace)
+	log.Debug("[CreatePod] the raw url is: ", rawUrl)
+
 	var pod *apiobject.Pod
-	if err := c.Bind(&pod); err != nil {
+	if err = c.Bind(&pod); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// 2. create the pod in the storage
-	namespace := c.Param("namespace")
-	log.Info("namespace: ", namespace)
+
+	// 2. save the pod's information in the storage
+	jsonBytes, err := pod.MarshalJSON()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	key := "/registry/pods/" + namespace + "/" + pod.Data.Name
+	log.Debug("[CreatePod] key: ", key)
+
+	err = p.StorageTool.Create(context.Background(), key, jsonBytes)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
 	// 3. return the pod to the client
+	c.JSON(http.StatusOK, pod)
 }
