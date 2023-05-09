d26fe08bc90a9e489a03142bc7229b3062f642e1
Jannnnne <2582964782@qq.com>
2023-05-09 20:11:00 +0800

    test:finish svccontroller test

--- pkg/controller/svccontroller.go
+++ pkg/controller/svccontroller.go
@@ -12,7 +12,7 @@ import (
 /* 主要工作：
 1. 监听service资源的创建。一旦service资源创建，为其分配唯一的cluster ip。
 2. 遍历pod列表，找到符合selector条件的pod，记录。创建endpoint。
-3. 监听pod创建。增加endpoint。
+3. 监听pod创建。增加endpoint。++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 4. 监听pod删除。删除endpoint。
 5. 监听pod更新。如果标签更改，删除/增加endpoint。
 6. 监听service资源的删除。删除对应endpoint。
