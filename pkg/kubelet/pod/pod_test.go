5773789e0bc0fb0b03f9034fecaebf7e6a725c4d
szy0127 <szy0127@sjtu.edu.cn>
2023-05-21 13:50:50 +0800

    feat: get pod metrics

--- pkg/kubelet/pod/pod_test.go
+++ pkg/kubelet/pod/pod_test.go
@@ -206,3 +206,67 @@ func TestPodCommunication(t *testing.T) {
 		t.Fatalf("rm containers failed")
 	}
 }
+
+func TestPodMetrics(t *testing.T) {
+	//should start etcd and flannld
+	namespace := "testpodns"
+	podName := "testpod"
+	pod := apiobject.Pod{
+		Data: apiobject.MetaData{Name: podName, Namespace: namespace},
+		Spec: apiobject.PodSpec{
+			Containers: []apiobject.Container{
+				{
+					Name:      "c1",
+					Image:     "ubuntu",
+					Command:   []string{"/root/test_mount/test_cpu"},
+					Resources: apiobject.Resources{Limits: apiobject.Limit{Cpu: "0.5"}},
+					VolumeMounts: []apiobject.VolumeMounts{
+						{
+							Name:      "test-volume",
+							MountPath: "/root/test_mount",
+						},
+					},
+				},
+				{
+					Name:    "c2",
+					Image:   "ubuntu",
+					Command: []string{"/root/test_mount/test_memory"},
+					VolumeMounts: []apiobject.VolumeMounts{
+						{
+							Name:      "test-volume",
+							MountPath: "/root/test_mount",
+						},
+					},
+				},
+			},
+			Volumes: []apiobject.Volumes{
+				{
+					Name:     "test-volume",
+					HostPath: apiobject.HostPath{Path: "/home/test_mount"},
+				},
+			},
+		}}
+	success, _ := CreatePod(pod, "192.168.1.13:8080")
+	if !success {
+		t.Fatalf("create pod failed")
+	}
+	time.Sleep(time.Second)
+	metrics := GetPodMetrics(namespace, podName)
+	if metrics == nil {
+		t.Fatalf("get pod metrics failed")
+	}
+	for _, c := range metrics.Containers {
+		if c.Name == podName+"-c1" {
+			cpu := c.Usage[apiobject.ResourceCPU]
+			if cpu > 600 || cpu <= 400 {
+				t.Fatalf("cpu metric error,%v", cpu)
+			}
+		} else {
+			memory := c.Usage[apiobject.ResourceMemory]
+			if memory < 200000000 {
+				t.Fatalf("memory metric error,%v", memory)
+			}
+		}
+	}
+	DeletePod(pod)
+}
