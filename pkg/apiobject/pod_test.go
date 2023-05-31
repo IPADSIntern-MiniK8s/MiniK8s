44b675990d2bb151f6a8197b47cae84a4c66825e
zhaohaoru <zhaohaoru@sjtu.edu.cn>
2023-05-31 20:29:14 +0800

    pref: podhandler logic

--- pkg/apiobject/pod_test.go
+++ pkg/apiobject/pod_test.go
@@ -1,6 +1,9 @@
 package apiobject
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestPod(t *testing.T) {
 	p := &Pod{
@@ -8,17 +11,25 @@ func TestPod(t *testing.T) {
 			Name: "test-pod",
 		},
 		Spec: PodSpec{
+			NodeSelector: map[string]string{"env": "test"},
 			Containers: []Container{
 				{Name: "test-container"},
 			},
 		},
 	}
 
+
+	workflowJson, err := json.MarshalIndent(p, "", "    ")
+	if err != nil {
+		t.Errorf("GenerateWorkflow failed, error marshalling replicas: %s", err)
+	}
+	t.Log(string(workflowJson))
+	
 	//expected := `{"metadata":{"name":"test-pod","labels":{}},"spec":{"containers":[{"name":"test-container"}]},"status":{}}`
 
-	_, err := p.MarshalJSON()
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
+	// _, err := p.MarshalJSON()
+	// if err != nil {
+	// 	t.Fatalf("unexpected error: %v", err)
+	// }
 
 }
